26e0414a2ba0f1da761e53b5364d912b981d9922
JonaiLive <jonaisoto.dev@gmail.com>
2022-08-01 15:25:04 -0400

    starting a quizapp in Ruby

---
+++ quizapp.rb
@@ -0,0 +1,19 @@
+puts "Welcome to Jonai's quiz app about nothing. You will be asked general knowledge questions and then be graded at the end. There will be 5 questions for you to answer. Good luck!"
+
+quiz_questions = {
+     "What was Meta Platforms Inc formerly known as?" => "a" , 
+     "Which English city is known as the Steel City?" => "a" ,
+     "Which former British colony was given back to China in 1997?" => "a" ,
+     "The logo for luxury car maker Porsche features which animal?" => "a" ,
+     "Which element is said to keep bones strong?" => "a" 
+  }
+
+
+# quiz_keys = quiz_questions.key(0)
+
+p quiz_questions.key("a")
+
+
+
+
+# hash within an array?
