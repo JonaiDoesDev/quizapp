a3341c4792880dd96936c7429cf1879dda0425d2
JonaiLive <jonaisoto.dev@gmail.com>
2022-08-08 14:22:05 -0400

    added the question logic

--- quizapp.rb
+++ quizapp.rb
@@ -1,17 +1,26 @@
 puts "Welcome to Jonai's quiz app about nothing. You will be asked general knowledge questions and then be graded at the end. There will be 5 questions for you to answer. Good luck!"
 
 quiz_questions = {
-     "What was Meta Platforms Inc formerly known as?" => "a" , 
-     "Which English city is known as the Steel City?" => "a" ,
-     "Which former British colony was given back to China in 1997?" => "a" ,
-     "The logo for luxury car maker Porsche features which animal?" => "a" ,
-     "Which element is said to keep bones strong?" => "a" 
+     "What was Meta Platforms Inc formerly known as?" => 1 , 
+     "Which English city is known as the Steel City?" => 2 ,
+     "Which former British colony was given back to China in 1997?" => 3 ,
+     "The logo for luxury car maker Porsche features which animal?" => 4,
+     "Which element is said to keep bones strong?" => 5 
   }
 
+user_answers = []
 
-# quiz_keys = quiz_questions.key(0)
+puts "Ok, first question!"
+puts quiz_questions.key(1)
+answer = gets.chomp
+user_answers << answer
 
-p quiz_questions.key("a")
+puts "Ok, second question!"
+puts quiz_questions.key(2)
+answer = gets.chomp
+user_answers << answer
+
+puts user_answers
 
 
 
